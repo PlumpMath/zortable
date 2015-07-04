4efb46ce219c9aff784ed154b148e9b2f1f4fe36
Sebastian Bensusan <sbensu@gmail.com>
2015-07-04 18:40:25 +0200

    Added docstrings

--- src/zortable/core.cljs
+++ src/zortable/core.cljs
@@ -1,35 +1,41 @@
 (ns zortable.core
   (:import [goog.events EventType])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
-  (:require [cljs.core.async.impl.protocols :as async-impl]
-            [clojure.set :as set]
+  (:require [clojure.set :as set]
             [cljs.core.async :as async :refer [>! <! chan put!]]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [goog.style :as style]
-            [goog.dom.classes :as classes]
             [goog.events :as events]
             [zortable.util :as u]))
 
 ;; ======================================================================  
 ;; Box
 
-(defn add-node [box]
+(defn add-node
+  "Adds the DOM node to the box as :node"
+  [box]
   {:pre [(some? (:eid box))]}
   (assoc box :node (.getElementById js/document (:eid box))))
 
-(defn add-size [box]
+(defn add-size
+  "Adds the box size as :width and :height from the DOM node"
+  [box]
   (let [n (aget (.-childNodes (:node box)) 0) 
         size (style/getSize n)]
     (assoc box :width (.-width size) :height (.-height size))))
 
-(defn add-pos [box]
+(defn add-pos
+  "Adds the box position as :left and :top from the DOM node"
+  [box]
   (let [final-pos (style/getPosition (:node box))
         left (.-x final-pos)
         top (.-y final-pos)]
     (assoc box :left left :top top)))
 
-(defn box-center [box]
+(defn box-center
+  "Calculates the box-center position"
+  [box]
   (letfn [(add [kb ks]
             (+ (kb box) (/ (ks box) 2)))]
     [(add :left :width) (add :top :height)]))
@@ -40,16 +46,24 @@
 (defn box-offset [pos box]
   (->> box topleft-pos (mapv - pos)))
 
-(defn element-inside? [class element]
+(defn element-inside?
+  "Finds if an element is inside another element with the specified class"
+  [class element]
   (some? (.closest element (str "." class))))
 
-(defn eid->box [id]
+(defn eid->box
+  "Finds the internal eid from the user given id"
+  [id]
   (add-size (add-pos (add-node {:eid id}))))
 
-(defn eid->id [id->eid ele-id]
+(defn eid->id
+  "Returns the user given id from the internal eid"
+  [id->eid ele-id]
   (some (fn [[id eid]] (if (= ele-id eid) id)) id->eid))
 
-(defn sort-by-pos [id->eid ids]
+(defn sort-by-pos
+  "Sorts the user-given ids by their vertical positions in the DOM into a vector"
+  [id->eid ids]
   (vec (sort-by (comp second box-center eid->box id->eid) ids)))
 
 ;; ====================================================================== 
@@ -87,11 +101,6 @@
 
 ;; ====================================================================== 
 ;; Wrapper Components
-
-(def valid-opts #{:box-view :box-filler})
-
-(defn clean-opts [opts]
-  (apply dissoc opts valid-opts))
 
 (defn sort-draggable [{:keys [box item]} owner opts]
   (reify
@@ -161,9 +170,6 @@
                     :react-key item-id})))
           sort)))))
 
-;; Listen to Events
-
-
 (defn zortable [{:keys [sort items] :as data} owner opts]
   (letfn [(init-state []
             {:start-pos []
@@ -185,8 +191,7 @@
                       (put! ch [tag {:pos (event->pos e)}]))]
               (merge
                 ;; State present during the whole lifecycle
-                {:zid (u/guid) ;; Unique to each loaded zortable
-                 :ids (om/value sort)
+                {:ids (om/value sort)
                  :id->eid (new-ids @sort) 
                  :ch ch
                  :listeners [(partial mouse-listen :drag)
@@ -228,24 +233,22 @@
                   to-create-ids (set/difference future-ids old-ids)
                   to-delete-ids (set/difference old-ids future-ids)
                   eids (->> (apply dissoc (get-local :id->eid) to-delete-ids)
-                         (merge (new-ids to-create-ids)))]
-              (next-state! {:ids @sort 
-                            :id->eid eids}))))
+                            (merge (new-ids to-create-ids)))]
+              (next-state! {:ids @sort :id->eid eids}))))
         om/IRenderState
-        (render-state [_ {:keys [ch ids id->eid zid box] :as state}]
+        (render-state [_ {:keys [ch ids id->eid box] :as state}]
           (let [moving-id (:id box)]
-            (apply dom/div #js {:id zid :className "zort-list"
+            (apply dom/div #js {:className "zort-list"
                                 :style zortable-styles} 
               (when (dragging? state)
-                (om/build sort-draggable
-                  {:box box 
-                   :item (items moving-id)}
+                (om/build sort-draggable {:box box :item (items moving-id)}
                   {:opts opts :react-key moving-id}))
               (map (fn [item-id]
                      (let [eid (id->eid item-id)
                            item (items item-id)]
                        (if (= item-id moving-id) 
-                         (om/build sort-filler {:item item :box box} {:opts opts})
+                         (om/build sort-filler {:item item :box box}
+                           {:opts opts})
                          (om/build sort-wrapper item
                            {:opts (assoc opts :ch ch)
                             :init-state {:eid eid :id item-id}
