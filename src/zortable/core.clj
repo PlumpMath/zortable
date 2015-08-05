81d6f7dc7a49e9db6b0591fc9c14b56021d78af6
Sebastian Bensusan <sbensu@gmail.com>
2015-08-06 01:13:22 +0200

    WIP: simple lets macro to combine signals

---
+++ src/zortable/core.clj
@@ -0,0 +1,18 @@
+(ns zortable.core)
+
+;; TODO: check for signals in bindings
+
+(defmacro lets [bindings expr]
+  (assert (vector? bindings) "The bindings should be a vector")
+  (assert (even? (count bindings)) "The bindings should be even")
+  (let [syms (mapv first (partition 2 bindings))
+        signals (mapv second (partition 2 bindings))
+        indexed-signals (vec (map-indexed vector signals))]
+    `(let [f# (fn ~syms
+                ~expr)
+           z# (atom {})]
+       (doseq [[i# s#] ~indexed-signals]
+         (add-watch s# (gensym)
+           (fn [~'_ ~'_ ~'_ state#]
+             (reset! z# (apply f# (assoc (mapv deref ~signals) i# state#))))))
+       z#)))
